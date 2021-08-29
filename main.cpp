be6bfefd9214db7c82b001fdd2c3b435a0b09a64
Philip Ritchey <philipritchey@users.noreply.github.com>
2021-08-29 19:47:39 +0000

    added i/o specification.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+/*
+input: n, integer from standard input
+output: print to standard output the goldbach composition of n "<n> = <p1> + <p2>"" or 
+"invalid"
+*/
+
 int main() {
   std::cout << "Hello World!\n";
 }
